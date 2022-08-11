abb0edbccced95d5f4ba8e1b6daf553d4794fb5a
miguelangel <miguelangelcdm@gmail.com>
2022-08-11 14:15:56 -0500

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    //Show Register/Create Form
+    public function create()
+    {
+        return view('users.register');
+    }
+    public function store(Request $request)
+    {
+        $formFields=$request->validate([
+            'name'=>['required','min:3'],
+            'email'=>['required','email',Rule::unique('users','email')],
+            'password'=>['required','confirmed','min:6']
+        ]);
+        //Hash Password->encrypting stored passwords
+        $formFields['password']=bcrypt($formFields['password']);
+
+        //Create User
+        $user=User::create($formFields);
+
+        //Login
+        auth()->login($user);
+        return redirect('/')->with('message','User created and logged in');
+
+    }
+    //log out
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/')->with('message','You ve been logged out!!');
+    }
+
+    //show login form
+    public function login()
+    {
+        return view('users.login');
+    }
+
+    //authenticate user
+    public function authenticate(Request $request)
+    {
+        $formFields=$request->validate([
+            'email'=>['required','email'],
+            'password'=>'required'
+        ]);
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+            return redirect('/')->with('message','You are now logged in!!');
+        }
+        return back()->withErrors(['email'=>'Invalid Credentials'])->onlyInput('email');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing
+// correr php artisan route:clear para refrescar cache si es que algun route no funciona como deberia!!
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+//Show Edit form
+Route::get('/listings/{listing}/edit',[ListingController::class,'edit'])->middleware('auth');
+
+//Update Listing
+Route::put('/listings/{listing}',[ListingController::class,'update'])->middleware('auth');
+
+//Delete Listing
+Route::delete('/listings/{listing}',[ListingController::class,'destroy'])->middleware('auth');
+
+//Show register/create form
+route::get('/register',[UserController::class,'create'])->middleware('guest');
+
+//Create New User
+route::post('/users',[UserController::class,'store']);
+
+//Log User Out
+route::post('/logout',[UserController::class,'logout'])->middleware('auth');
+
+//Show Login Form
+Route::get('/login',[UserController::class,'login'])->name('login')->middleware('guest');
+
+//Log In User
+route::post('/users/authenticate',[UserController::class,'authenticate']);
+
+//Manage Listings
+Route::get('/listings/manage',[ListingController::class,'manage'])->middleware('auth');
+
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
